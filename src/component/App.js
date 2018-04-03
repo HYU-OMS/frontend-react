c866a9a572618e6221c6854c58baa8736efb0501
Kihoon Han <khhan1993@gmail.com>
2018-04-03 12:10:55 +0000

    2018년 4월 3일까지 작업 결과 commit

--- src/component/App.js
+++ src/component/App.js
@@ -1,22 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { CssBaseline } from 'material-ui';
 
 import Home from './home/home';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      
-    };
-  }
-  
   render() {
     return (
       <div>
-        <CssBaseline />
         <Switch>
           <Route path="/home" component={Home} />
           <Redirect to="/home" /> 
