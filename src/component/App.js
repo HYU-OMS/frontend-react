9e44ac2e06bc247336d23b3e631d2992f453c543
Kihoon Han <khhan1993@gmail.com>
2018-04-05 09:56:51 +0900

    Material UI 적용 테스트중

--- src/component/App.js
+++ src/component/App.js
@@ -1,24 +1,161 @@
-import React from 'react';
+import * as React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { CssBaseline } from 'material-ui';
-import { AppBar } from 'material-ui';
+import { withStyles } from 'material-ui/styles';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  IconButton,
+  Hidden
+} from 'material-ui';
+import InboxIcon from 'material-ui-icons/MoveToInbox';
+import DraftsIcon from 'material-ui-icons/Drafts';
+import StarIcon from 'material-ui-icons/Star';
+import SendIcon from 'material-ui-icons/Send';
+import MenuIcon from 'material-ui-icons/Menu';
 
 import Home from './home/home';
 
+const drawerWidth = 240;
+
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    height: '100%',
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mobileOpen: false,
+    };
+
+    this.handleDrawerToggle = this.handleDrawerToggle.bind(this);
+  }
+
+  handleDrawerToggle = () => {
+    this.setState({
+      mobileOpen: !this.state.mobileOpen
+    });
+  };
+
   render() {
-    return (
+    const { classes, theme } = this.props;
+
+    const appbar = (
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={this.handleDrawerToggle}
+            className={classes.navIconHide}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" noWrap>
+            HYU OMS
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+
+    const drawer = (
       <div>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+        </List>
+        <Divider />
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
         <CssBaseline />
-        <AppBar position="static">
+        {appbar}
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
           <Switch>
-          <Route path="/home" component={Home} />
-          <Redirect to="/home" />
-        </Switch>
-        </AppBar>
+            <Route path="/home" component={Home} />
+            <Redirect to="/home" />
+          </Switch>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
