21e4fac6c2d37b63c607ebf68aee97a519cb4913
Kihoon Han <khhan1993@gmail.com>
2019-01-27 00:00:45 +0900

    주문 관련 기능 추가

--- src/component/order/list.js
+++ src/component/order/list.js
@@ -1,22 +1,234 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import {
+  Paper,
+  Typography,
+  Button,
+  Hidden,
+  Table, TableBody, TableCell, TableHead, TableRow
+} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
 
 const styles = (theme) => ({
-
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  tablecell: {
+    padding: "15px",
+    textAlign: "center"
+  },
+  textField: {
+    paddingLeft: "15px",
+    paddingRight: "15px",
+    paddingTop: "10px",
+    paddingBottom: "10px"
+  },
+  button: {
+    margin: theme.spacing.unit,
+    fontSize: '12px',
+    width: '30px',
+    height: '30px'
+  },
+  buttonAlign: {
+    textAlign: 'center'
+  }
 });
 
 class OrderList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      "list": [],
+      "pagination": [],
+      "cur_page": 1,
+      "is_list_loading": true,
+    };
+  }
+
+  componentDidMount() {
+    this.getOrderList(1);
+  }
+
+  getOrderList(page) {
+    const url = this.props.api_url + "/v1/order?group_id=" +
+      (this.props.group_id).toString() + "&page=" + page.toString();
+    const headers = {
+      "Authorization": "Bearer " + this.props.jwt
+    };
+
+    this.setState({
+      "is_list_loading": true
+    });
+
+    axios.get(url, {"headers": headers})
+      .then((response) => {
+        this.setState({
+          "list": response.data.list,
+          "pagination": response.data.pagination,
+          "is_list_loading": false
+        });
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+        this.setState({
+          "is_list_loading": false
+        });
+      });
+  }
+
+  handleGetOrderListClick = (page_num) => {
+    this.setState({
+      "cur_page": page_num,
+      "remaining_refresh_time": 9
+    });
+
+    this.getOrderList(page_num);
+  };
+
+  getDateString(dateInfo) {
+    const dateObj = new Date(dateInfo);
+    const dateString = dateObj.getFullYear() + "년 " +
+      (dateObj.getMonth() + 1).toString() + "월 " +
+      dateObj.getDate() + "일 " +
+      dateObj.getHours() + "시 " +
+      dateObj.getMinutes() + "분";
+
+    return dateString;
+  }
+
+  getStatusIcon(status) {
+    switch(status) {
+      case 1:
+        return (<CheckIcon />);
+
+      case 0:
+        return (<HourglassEmptyIcon />);
+
+      case -1:
+        return (<CloseIcon />);
+
+      default:
+        return null;
+    }
+  }
+
+  handleGetOrderInfo(orderItem) {
+    orderItem.is_loading = true;
+    this.setState({
+      "list": this.state.list
+    });
+
+    const url = this.props.api_url + "/v1/order/" +
+      (orderItem.id).toString();
+    const headers = {
+      "Authorization": "Bearer " + this.props.jwt
+    };
+
+    axios.get(url, {"headers": headers})
+      .then((response) => {
+        orderItem.is_loading = false;
+        this.setState({
+          "list": this.state.list
+        });
+
+        let alert_msg = "주문번호 : " + (response.data.order_id).toString() + "\n";
+        alert_msg += "테이블 : " + (orderItem.table_id).toString() + "\n";
+        alert_msg += "시간 : " + this.getDateString(orderItem.created_at) + "\n";
+
+        alert_msg += "\n[일반메뉴]\n";
+        for(const menu_info of response.data.order_menus) {
+          alert_msg += (menu_info['name'] + " : " + menu_info['amount'] + "\n");
+        }
+
+        alert_msg += "\n[세트메뉴]\n";
+        for(const setmenu_info of response.data.order_setmenus) {
+          alert_msg += (setmenu_info['name'] + " : " + setmenu_info['amount'] + "\n");
+        }
+
+        alert(alert_msg);
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  }
+
   render() {
     const { classes } = this.props;
 
+    const rowItems = this.state.list.map((rowItem) =>
+      <TableRow key={rowItem.id}>
+        <TableCell className={classes.tablecell}>{rowItem.id}</TableCell>
+        <Hidden xsDown>
+          <TableCell className={classes.tablecell}>{rowItem.name}</TableCell>
+        </Hidden>
+        <TableCell className={classes.tablecell} onClick={(e) => this.handleGetOrderInfo(rowItem)}>
+          <TouchAppIcon />
+        </TableCell>
+        <TableCell className={classes.tablecell}>{rowItem.total_price}</TableCell>
+        <Hidden xsDown>
+          <TableCell className={classes.tablecell}>{rowItem.table_id}</TableCell>
+          <TableCell className={classes.tablecell}>{this.getDateString(rowItem.created_at)}</TableCell>
+        </Hidden>
+        <TableCell className={classes.tablecell}>{this.getStatusIcon(rowItem.status)}</TableCell>
+      </TableRow>
+    );
+
+    const pageItems = this.state.pagination.map((page) =>
+      <Button key={page.num} variant="fab" mini color={(page.current === true) ? 'primary' : 'default'} className={classes.button} onClick={(e) => this.handleGetOrderListClick(page.num)}>
+        {page.text}
+      </Button>
+    );
+
     return (
       <div>
-        <Typography component="p">더 나은 서비스를 제공하기 위해 개편 준비 중입니다.</Typography>
-        <Typography component="p">To provide better service, we are preparing for new version.</Typography>
+        <Typography variant="headline" align="center">주문 내역</Typography>
+        <Paper className={classes.root}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.tablecell}>#</TableCell>
+                <Hidden xsDown>
+                  <TableCell className={classes.tablecell}>주문자명</TableCell>
+                </Hidden>
+                <TableCell className={classes.tablecell}>내역</TableCell>
+                <TableCell className={classes.tablecell}>가격</TableCell>
+                <Hidden xsDown>
+                  <TableCell className={classes.tablecell}>테이블</TableCell>
+                  <TableCell className={classes.tablecell}>시간</TableCell>
+                </Hidden>
+                <TableCell className={classes.tablecell}>상태</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rowItems}
+            </TableBody>
+          </Table>
+          <div className={classes.buttonAlign}>
+            {pageItems}
+          </div>
+        </Paper>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(OrderList);
+const mapStateToProps = (state) => {
+  return {
+    "jwt": state.auth.jwt,
+    "api_url": state.auth.api_url,
+    "group_id": state.auth.group_id
+  };
+};
+
+export default connect(
+  mapStateToProps
+)(withStyles(styles)(OrderList));
--- src/component/order/request.js
+++ src/component/order/request.js
@@ -1,22 +1,352 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import {
+  Paper,
+  Typography,
+  Button,
+  Hidden,
+  Table, TableBody, TableCell, TableHead, TableRow,
+  TextField,
+  FormControl,
+  Card, CardActions, CardContent
+} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
 const styles = (theme) => ({
-
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  tablecell: {
+    padding: "15px",
+    textAlign: "center"
+  },
+  textField: {
+    paddingLeft: "15px",
+    paddingRight: "15px",
+    paddingTop: "10px",
+    paddingBottom: "10px"
+  }
 });
 
 class OrderRequest extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      "menu_list": [],
+      "setmenu_list": [],
+      "is_menu_list_loading": true,
+      "is_setmenu_list_loading": true,
+      "is_in_order_processing": false,
+      "table_name": "",
+      "total_price": 0
+    };
+  }
+
+  componentDidMount() {
+    this.getMenuList();
+    this.getSetmenuList();
+  }
+
+  getMenuList = () => {
+    const url = this.props.api_url + "/v1/menu?group_id=" + (this.props.group_id).toString();
+    const headers = {
+      "Authorization": "Bearer " + this.props.jwt
+    };
+
+    this.setState({
+      "is_menu_list_loading": true
+    });
+
+    axios.get(url, {"headers": headers})
+      .then((response) => {
+        this.setState({
+          "menu_list": response.data,
+          "is_menu_list_loading": false
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        //alert(error.response.data.message);
+        this.setState({
+          "is_menu_list_loading": false
+        });
+      });
+  };
+
+  getSetmenuList = () => {
+    const url = this.props.api_url + "/v1/setmenu?group_id=" + (this.props.group_id).toString();
+    const headers = {
+      "Authorization": "Bearer " + this.props.jwt
+    };
+
+    this.setState({
+      "is_setmenu_list_loading": true
+    });
+
+    axios.get(url, {"headers": headers})
+      .then((response) => {
+        this.setState({
+          "setmenu_list": response.data,
+          "is_setmenu_list_loading": false
+        });
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+        this.setState({
+          "is_setmenu_list_loading": false
+        });
+      });
+  };
+
+  handleMenuItemAdjust = (menuItem, value) => {
+    if(menuItem.is_enabled !== 1) {
+      return;
+    }
+
+    if(isNaN(menuItem.amount)) {
+      menuItem.amount = 0;
+    }
+
+    menuItem.amount += value;
+
+    this.setState({
+      "menu_list": this.state.menu_list,
+      "total_price": this.state.total_price + (value * menuItem.price)
+    });
+  };
+
+  handleSetmenuItemAdjust = (setmenuItem, value) => {
+    if(setmenuItem.is_enabled !== 1) {
+      return;
+    }
+
+    if(isNaN(setmenuItem.amount)) {
+      setmenuItem.amount = 0;
+    }
+
+    setmenuItem.amount += value;
+
+    this.setState({
+      "setmenu_list": this.state.setmenu_list,
+      "total_price": this.state.total_price + (value * setmenuItem.price)
+    });
+  };
+
+  handleTableNameChange = (e) => {
+    this.setState({
+      "table_name": e.target.value
+    });
+  };
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    const url = this.props.api_url + "/v1/order";
+    const headers = {
+      "Authorization": "Bearer " + this.props.jwt
+    };
+
+    const menu_list = [];
+    for(const menu of this.state.menu_list) {
+      if(!isNaN(menu.amount) && menu.amount !== 0) {
+        menu_list.push({
+          "id": menu.id,
+          "amount": menu.amount
+        });
+      }
+    }
+
+    const setmenu_list = [];
+    for(const setmenu of this.state.setmenu_list) {
+      if(!isNaN(setmenu.amount) && setmenu.amount !== 0) {
+        setmenu_list.push({
+          "id": setmenu.id,
+          "amount": setmenu.amount
+        });
+      }
+    }
+
+    if(menu_list.length === 0 && setmenu_list.length === 0) {
+      alert("선택된 메뉴/세트메뉴 가 하나도 없습니다!");
+    } else {
+      this.setState({
+        "is_in_order_processing": true
+      });
+
+      axios.post(url, {
+        "group_id": this.props.group_id,
+        "table_id": this.state.table_name,
+        "menu_list": menu_list,
+        "setmenu_list": setmenu_list
+      }, {"headers": headers}).then((response) => {
+        const alert_msg = "주문 요청이 완료되었습니다.\n" +
+          "주문번호 : " + (response.data.order_id).toString() + "\n" +
+          "총 금액 : " + (response.data.total_price).toString();
+
+        alert(alert_msg);
+
+        for(const menu of this.state.menu_list) {
+          if(!isNaN(menu.amount)) {
+            delete menu.amount;
+          }
+        }
+
+        for(const setmenu of this.state.setmenu_list) {
+          if(!isNaN(setmenu.amount)) {
+            delete setmenu.amount;
+          }
+        }
+
+        this.setState({
+          "is_in_order_processing": false,
+          "table_name": "",
+          "total_price": 0,
+          "menu_list": this.state.menu_list,
+          "setmenu_list": this.state.setmenu_list
+        });
+      }).catch((error) => {
+        alert(error.response.data.message);
+
+        this.setState({
+          "is_in_order_processing": false
+        });
+
+        this.getMenuList();
+        this.getSetmenuList();
+      });
+    }
+  };
+
   render() {
     const { classes } = this.props;
 
+    const menuItems = this.state.menu_list.map((menuItem) =>
+      <TableRow hover={true} key={menuItem.id} style={{ opacity: (parseInt(menuItem.is_enabled, 10) !== 1) ? 0.25 : 1 }}>
+        <TableCell className={classes.tablecell}>{menuItem.name}</TableCell>
+        <TableCell className={classes.tablecell}>{menuItem.price}</TableCell>
+        <TableCell className={classes.tablecell}>{menuItem.amount}</TableCell>
+        <TableCell onClick={(e) => this.handleMenuItemAdjust(menuItem, 1)} className={classes.tablecell}>
+          <AddIcon />
+        </TableCell>
+        <TableCell onClick={(e) => this.handleMenuItemAdjust(menuItem, -1)} className={classes.tablecell}>
+          <RemoveIcon />
+        </TableCell>
+      </TableRow>
+    );
+
+    const setmenuItems = this.state.setmenu_list.map((setmenuItem) =>
+      <TableRow hover={true} key={setmenuItem.id} style={{ opacity: (parseInt(setmenuItem.is_enabled, 10) !== 1) ? 0.25 : 1 }}>
+        <TableCell className={classes.tablecell}>{setmenuItem.name}</TableCell>
+        <TableCell className={classes.tablecell}>{setmenuItem.price}</TableCell>
+        <TableCell className={classes.tablecell}>{setmenuItem.amount}</TableCell>
+        <TableCell onClick={(e) => this.handleSetmenuItemAdjust(setmenuItem, 1)} className={classes.tablecell}>
+          <AddIcon />
+        </TableCell>
+        <TableCell onClick={(e) => this.handleSetmenuItemAdjust(setmenuItem, -1)} className={classes.tablecell}>
+          <RemoveIcon />
+        </TableCell>
+      </TableRow>
+    );
+
     return (
       <div>
-        <Typography component="p">더 나은 서비스를 제공하기 위해 개편 준비 중입니다.</Typography>
-        <Typography component="p">To provide better service, we are preparing for new version.</Typography>
+        <div>
+          <Typography variant="headline" align="center">메뉴 목록</Typography>
+          <Paper className={classes.root}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.tablecell}>이름</TableCell>
+                  <TableCell className={classes.tablecell}>가격</TableCell>
+                  <TableCell className={classes.tablecell}>수량</TableCell>
+                  <TableCell className={classes.tablecell} colSpan="2">수량 조절</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {menuItems}
+              </TableBody>
+            </Table>
+          </Paper>
+        </div>
+
+        <br/>
+
+        <div>
+          <Typography variant="headline" align="center">세트메뉴 목록</Typography>
+          <Paper className={classes.root}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.tablecell}>이름</TableCell>
+                  <TableCell className={classes.tablecell}>가격</TableCell>
+                  <TableCell className={classes.tablecell}>수량</TableCell>
+                  <TableCell className={classes.tablecell} colSpan="2">수량 조절</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {setmenuItems}
+              </TableBody>
+            </Table>
+          </Paper>
+        </div>
+
+        <br/>
+
+        <div>
+          <Typography variant="headline" align="center">테이블 이름</Typography>
+          <Paper className={classes.root}>
+            <FormControl fullWidth>
+              <TextField
+                placeholder="테이블 이름 입력"
+                className={classes.textField}
+                value={this.state.table_name}
+                onChange={this.handleTableNameChange}
+                fullWidth
+              />
+            </FormControl>
+          </Paper>
+        </div>
+
+        <br/><hr/><br/>
+
+        <div>
+          <Card>
+            <CardContent>
+              <Typography color="textSecondary">
+                주문 정보
+              </Typography>
+              <Typography variant="title">
+                총 가격 : {this.state.total_price}
+              </Typography>
+              <Typography variant="title">
+                테이블 이름 : {this.state.table_name}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button fullWidth onClick={this.handleOnSubmit} color="primary" variant="contained">주문 요청</Button>
+            </CardActions>
+          </Card>
+        </div>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(OrderRequest);
+const mapStateToProps = (state) => {
+  return {
+    "jwt": state.auth.jwt,
+    "api_url": state.auth.api_url,
+    "group_id": state.auth.group_id
+  }
+};
+
+export default connect(
+  mapStateToProps
+)(withStyles(styles)(OrderRequest));
