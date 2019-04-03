6edd581e95076d0d6b68f81e0188b80890f3141c
Kihoon Han <khhan1993@gmail.com>
2019-04-03 11:47:09 +0900

    홈 화면 메세지 수정

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -21,8 +21,18 @@ class Home extends React.Component {
         <br/>
 
         <Typography component="p">
+          <ul>
+            <li>2019년도 한양대학교 봄 축제는 진행되지 않을 예정이라고 합니다.</li>
+            <li>HYU-OMS 는 계속 서비스를 할 예정이지만 올해의 경우 별도의 서버 사양 업그레이드는 진행하지 않을 예정입니다. 이용에 참고하시기 바랍니다.</li>
+          </ul>
+        </Typography>
+
+        <br/>
+
+        <Typography component="p">
           더 나은 서비스를 위해 개편 중입니다.<br/>
-          <strong>문의</strong>: <a href="mailto: kordreamfollower@gmail.com">kordreamfollower@gmail.com</a>
+          <strong>문의</strong>: <a href="mailto: kordreamfollower@gmail.com">kordreamfollower@gmail.com</a><br/>
+          <strong>GitHub Repository</strong>: <a href="https://github.com/HYU-OMS">HYU-OMS Dev Group</a>
         </Typography>
       </div>
     );
