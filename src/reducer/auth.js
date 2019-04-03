30e08913d6c586b398c2681b357781822515e58f
Kihoon Han <khhan1993@gmail.com>
2019-04-03 12:09:12 +0900

    Custom API URL 지정 가능하도록 코드 버그 수정

--- src/reducer/auth.js
+++ src/reducer/auth.js
@@ -5,10 +5,24 @@ const initialState = {
   "group_id": (localStorage.getItem("group_id") !== null) ? parseInt(localStorage.getItem("group_id"), 10) : null,
   "role": (localStorage.getItem("role") !== null) ? parseInt(localStorage.getItem("role"), 10) : null,
   "signup_code": (localStorage.getItem("signup_code") !== null) ? localStorage.getItem("signup_code") : "",
-  "api_url": (process.env.NODE_ENV === 'development') ?
-    ((Boolean(process.env.API_URL) === false) ?
-      'http://127.0.0.1:8080' : process.env.REACT_APP_API_URL)
-    : 'https://api.hyu-oms.com'
+  "api_url": (() => {
+    if(process.env.NODE_ENV === 'development') {
+      if(Boolean(process.env.REACT_APP_API_URL) === false) {
+        return 'http://127.0.0.1:8080';
+      }
+      else {
+        return process.env.REACT_APP_API_URL;
+      }
+    }
+    else {
+      if(Boolean(process.env.REACT_APP_API_URL) === false) {
+        return 'https://api.hyu-oms.com';
+      }
+      else {
+        return process.env.REACT_APP_API_URL;
+      }
+    }
+  })()
 };
 
 const auth = (state = initialState, action) => {
